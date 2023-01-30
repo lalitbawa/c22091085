737c2ada4602ebebb43e58af67c930b4d029c00d
lalitbawa <101063178+lalitbawa@users.noreply.github.com>
2023-01-30 06:05:37 +0000

    Delete CardiffCMT120 directory

--- CardiffCMT120/website/static/index.js
+++
@@ -1,8 +0,0 @@
-// Mobile navbar toggle
-
-const toggle_button = document.getElementById("toggle-button")
-const nav_link = document.getElementsByClassName("navbar-links1")[0]
-
-toggle_button.addEventListener('click',()=>{
-    nav_link.classList.toggle('active')
-})
